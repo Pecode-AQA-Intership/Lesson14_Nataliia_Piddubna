04bcaa0f8e01701cff9d03c98131310561a5fa95
Nataliia <felix.fortuna.nata@gmail.com>
2022-02-11 00:01:16 +0200

    Lesson 5. Tasks 1&2

--- cypress/integration/Lesson15_Cypress_spec.js
+++ cypress/integration/Lesson15_Cypress_spec.js
@@ -1,6 +1,5 @@
 import {faker} from '@faker-js/faker';
 
-// 1. Create a new user and verify that user was added;
 const MAIN_URL = 'https://demoqa.com/webtables';
 const SELECTOR_DATA = {
   buttonAdd:'#addNewRecordButton',
@@ -11,16 +10,25 @@ const SELECTOR_DATA = {
   salary: '#salary',
   department: '#department',
   buttonSubmit: '#submit',
-  tables: '.ReactTable'
+  tables: '.ReactTable',
+  buttonEdit: 'span#edit-record-4',
+  editTable: 'div.rt-thead.-header'
+
 };//selector's array for a convenient usage
 
 const FAKER_DATA = {
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
   email: faker.internet.email(),
-  age: faker.random.number(125),
+  age: faker.random.number(99),
   salary: faker.random.number(84000),
-  department: faker.commerce.department()
+  department: faker.commerce.department(),
+  editFirstName: faker.name.firstName(),
+  editLastName: faker.name.lastName(),
+  editEmail: faker.internet.email(),
+  editAge: faker.random.number(78),
+  editSalary: faker.random.number(89000),
+  editDepartment: faker.commerce.department()
 };//faker's array in a convenient form
 
 describe('Create a new user in box "Web Tables"', () => {
@@ -28,26 +36,38 @@ describe('Create a new user in box "Web Tables"', () => {
     cy.visit(MAIN_URL);
   }); //open site as a pre-conditions
 
-  it('Create a new user', () => { 
-    cy.get(SELECTOR_DATA.buttonAdd).click({timeout: 1000});//click the button 'Add'
+  it('Create a new user and verify that user was added', () => { 
+    cy.get(SELECTOR_DATA.buttonAdd).click({timeout: 2000});//click the button 'Add'
     cy.get(SELECTOR_DATA.firstName).type(FAKER_DATA.firstName).should('have.value', FAKER_DATA.firstName);//fill in the box 'First Name' 
     cy.get(SELECTOR_DATA.lastName).type(FAKER_DATA.lastName).should('have.value', FAKER_DATA.lastName); //fill in the box 'Last Name'
     cy.get(SELECTOR_DATA.email).type(FAKER_DATA.email).should('have.value', FAKER_DATA.email);//fill in the box 'Email'
-    cy.get(SELECTOR_DATA.age).type(FAKER_DATA.age).should('have.value', FAKER_DATA.age);//fill in the box 'Age'
+    cy.get(SELECTOR_DATA.age).type(FAKER_DATA.age).should('have.value', FAKER_DATA.age, ({timeout: 1000}));//fill in the box 'Age'
     cy.get(SELECTOR_DATA.salary).type(FAKER_DATA.salary).should('have.value', FAKER_DATA.salary);//fill in the box 'Salary'
     cy.get(SELECTOR_DATA.department).type(FAKER_DATA.department).should('have.value', FAKER_DATA.department);//fill in the box 'Department'
-    cy.get(SELECTOR_DATA.buttonSubmit).click({timeout: 3000});//click the button 'Add' 
+    cy.get(SELECTOR_DATA.buttonSubmit).click({timeout: 3000});//click the button 'Submit' 
     cy.get(SELECTOR_DATA.tables).contains(FAKER_DATA.email).should("be.visible");//check availability the user via e-mail
     cy.get(SELECTOR_DATA.tables).contains(FAKER_DATA.email).parent().then((allDataInRow) => {
         cy.get(allDataInRow)
           .should('contain', FAKER_DATA.firstName).and('contain', FAKER_DATA.lastName).and('contain', FAKER_DATA.age)
           .and('contain', FAKER_DATA.salary).and('contain', FAKER_DATA.department); //Verify that user was added. 
-          //Firstly,find e-mail selector, its parent for getting all elements from row
-    });
+    //Firstly,find e-mail selector, its parent for getting all elements from row
+  });
+});
+    it('Edit user and check that each field is editable', () => { 
+    cy.get(SELECTOR_DATA.buttonEdit).click({timeout: 5000});//click the button 'Edit'.'Edit user and check if editing is completed'
+    cy.get(SELECTOR_DATA.firstName).clear().type(FAKER_DATA.editFirstName);//edit the box 'First Name' 
+    cy.get(SELECTOR_DATA.lastName).clear().type(FAKER_DATA.editLastName); //edit the box 'Last Name'
+    cy.get(SELECTOR_DATA.email).clear().type(FAKER_DATA.editEmail);//edit the box 'Email'
+    cy.get(SELECTOR_DATA.age).clear().type(FAKER_DATA.editAge);//edit the box 'Age'
+    cy.get(SELECTOR_DATA.salary).clear().type(FAKER_DATA.editSalary);//edit the box 'Salary'
+    cy.get(SELECTOR_DATA.department).clear().type(FAKER_DATA.editDepartment);
+    cy.get(SELECTOR_DATA.buttonSubmit).click({timeout: 3000});//click the button 'Submit'
+    cy.get(SELECTOR_DATA.editTable).parent().should('contain',FAKER_DATA.editFirstName).and('contain',FAKER_DATA.editLastName)
+    .and('contain',FAKER_DATA.editEmail).and('contain',FAKER_DATA.editAge).and('contain',FAKER_DATA.editSalary)
+    .and('contain',FAKER_DATA.editDepartment)
   });
 });
 
-// 2. Edit user and check that each field is editable;
 
 // 3. Delete user from the table and check that user 
 // was deleted;
